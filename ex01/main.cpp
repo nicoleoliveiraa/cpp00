719dd0b85bf6f4b4327707baa14a0a64ca5a93aa
nicoleoliveiraa <nicole.s.oliveira26@gmail.com>
2024-10-01 14:11:22 +0100

    feat: Add user input functionality to main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsouza-o <nsouza-o@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:06:27 by nsouza-o          #+#    #+#             */
-/*   Updated: 2024/10/01 12:07:44 by nsouza-o         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:00:33 by nsouza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int main()
 {
 	PhoneBook PhoneBook;
-	
+	std::string command;
+
+	std::getline(std::cin, command);
+	while ()
 	PhoneBook.add_contact();
 	return (0);
 }
